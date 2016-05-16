0411360c4fdc5eed7b65ee36fffa597df79f7e27
Jere Menichelli <jmenichelli@gmail.com>
2016-05-16 10:59:29 -0300

    Generate distribution files

--- dist/hunt.js
+++ dist/hunt.js
@@ -5,7 +5,7 @@
             return factory();
         });
     } else if (typeof exports === 'object') {
-        module.exports = factory;
+        module.exports = factory();
     } else {
         root.hunt = factory();
     }
@@ -13,48 +13,14 @@
     'use strict';
 
     var huntedElements = [],
-        ticking = false,
         viewport = window.innerHeight,
-        y = 0;
+        THROTTLE_INTERVAL = 100;
 
-    // request animation frame and cancel animation frame vendors
-    var rAF = (function() {
-        return window.requestAnimationFrame ||
-            window.webkitRequestAnimationFrame ||
-            window.mozRequestAnimationFrame;
-    })();
-
-    /*
-     * Returns distance between element and window top
-     * @method getOffsetTop
-     * @param {Node} element
-     */
-    var getOffsetTop = function(element) {
-        var top = element.offsetTop,
-            offsetParent = element.offsetParent;
-
-        // escalate offset parent assignation
-        while (offsetParent) {
-            top += offsetParent.offsetTop;
-            offsetParent = offsetParent.offsetParent;
-        }
-
-        return top;
-    };
-
-    /*
-     * Returns distance vertically scrolled
-     * @method getScrollY
-     */
-    var getScrollY = function() {
-        return viewport + window.scrollY || window.pageYOffset;
-    };
-
-    /*
+    /**
      * Constructor for element that should be hunted
      * @constructor Hunted
      * @param {Node} element
-     * @param {Object} options
+     * @param {Object} config
      */
     var Hunted = function(element, config) {
         this.element = element;
@@ -62,20 +28,11 @@
         // instantiate element as not visible
         this.visible = false;
 
-        // assign metrics of the first time
-        this.updateMetrics();
-
         for (var prop in config) {
             if (config.hasOwnProperty(prop)) {
                 this[prop] = config[prop];
             }
         }
-    };
-
-    // assign or updates instance metrics
-    Hunted.prototype.updateMetrics = function() {
-        this.height = this.element.clientHeight;
-        this.top = getOffsetTop(this.element);
     };
 
     // by default offset is zero
@@ -90,7 +47,7 @@
     // fallback out function to avoid sanity check
     Hunted.prototype.out = function() {};
 
-    /*
+    /**
      * Adds one or more elements to the hunted elements array
      * @method add
      * @param {Array|Node} elements
@@ -101,7 +58,7 @@
         if (elements instanceof Node === false
                 && typeof elements.length !== 'number'
                 || typeof options !== 'object') {
-            throw new TypeError('You must pass an element or a collection of them and an options object');
+            throw new TypeError('Arguments must be an element or a list of them and an object');
         }
 
         // treat single node as array
@@ -123,92 +80,88 @@
         i = len = null;
     };
 
-    /*
+    /**
      * Updates viewport and elements metrics
      * @method updateMetrics
      */
     var updateMetrics = function() {
         viewport = window.innerHeight;
-        y = getScrollY();
 
-        var i = 0,
-            len = huntedElements.length;
-
-        for (; i < len; i++) {
-            huntedElements[i].updateMetrics();
-        }
-
-        i = len = null;
+        // check if new elements became visible
+        huntElements();
     };
 
-    /*
+    /**
      * Checks if hunted elements are visible and resets ticking
      * @method huntElements
      */
     var huntElements = function() {
         var len = huntedElements.length,
-            hunted;
+            hunted,
+            rect;
 
-        if (len > 0) {
-            y = getScrollY();
+        while (len) {
+            --len;
 
-            while (len) {
-                --len;
+            hunted = huntedElements[len];
+            rect = hunted.element.getBoundingClientRect();
 
-                hunted = huntedElements[len];
+            /*
+             * trigger (in) event if element comes from a non visible state and the scrolled viewport has
+             * reached the visible range of the element without exceeding it
+             */
+            if (!hunted.visible
+                    && rect.top - hunted.offset < viewport
+                    && rect.top >= -(rect.height + hunted.offset)) {
+                hunted.in.apply(hunted.element);
+                hunted.visible = true;
+            }
 
-                /*
-                 * trigger (in) event if element comes from a non visible state and the scrolled viewport has
-                 * reached the visible range of the element without exceeding it
-                 */
-                if (!hunted.visible
-                        && y > hunted.top - hunted.offset
-                        && y < hunted.top + hunted.height + viewport + hunted.offset) {
-                    hunted.in.apply(hunted.element);
-                    hunted.visible = true;
-                }
+            /*
+             * trigger (out) event if element comes from a visible state and it's out of the visible
+             * range its bottom or top limit
+             */
+            if (hunted.visible
+                    && (rect.top - hunted.offset >= viewport
+                    || rect.top <= -(rect.height + hunted.offset))) {
+               hunted.out.apply(hunted.element);
+               hunted.visible = false;
 
-                /*
-                 * trigger (out) event if element comes from a visible state and it's out of the visible
-                 * range its bottom or top limit
-                 */
-                if (hunted.visible
-                        && (y <= hunted.top - hunted.offset
-                        || y >= hunted.top + hunted.height + viewport + hunted.offset)) {
-                   hunted.out.apply(hunted.element);
-                   hunted.visible = false;
-
-                    // when hunting should not persist kick element out
-                    if (!hunted.persist) {
-                       huntedElements.splice(len, 1);
-                    }
+                // when hunting should not persist kick element out
+                if (!hunted.persist) {
+                   huntedElements.splice(len, 1);
                 }
             }
         }
 
-        // reset ticking
-        ticking = false;
-
         hunted = len = null;
     };
 
-    /*
-     * Delays action until next available frame according to technic
-     * exposed by Paul Lewis http://www.html5rocks.com/en/tutorials/speed/animations/
-     * @method debounceHunt
+    /**
+     * Prevents overcall during global specified interval
+     * @method throttle
+     * @param {Function} fn
+     * @returns {Function}
      */
-    var debounceHunt = function() {
-        if (!ticking) {
-            rAF(huntElements);
-        }
-        ticking = true;
+    var throttle = function(fn) {
+        var timer = null;
+
+        return function () {
+            if (timer) {
+                return;
+            }
+            timer = setTimeout(function () {
+                fn.apply(this, arguments);
+                timer = null;
+            }, THROTTLE_INTERVAL);
+        };
     };
 
     // on resize update viewport metrics
-    window.addEventListener('resize', updateMetrics);
+    window.addEventListener('resize', throttle(updateMetrics));
 
     // on scroll check for elements position and trigger methods
-    window.addEventListener('scroll', debounceHunt);
+    window.addEventListener('scroll', throttle(huntElements));
 
     return add;
 });
