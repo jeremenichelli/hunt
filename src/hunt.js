d42c84b338e16650b15ce7b00d862ada87d66f91
Daniel Ruf <DanielRuf@users.noreply.github.com>
2017-05-11 15:33:51 +0200

    stop hunting if persist ist false and out is noop

--- src/hunt.js
+++ src/hunt.js
@@ -135,6 +135,16 @@
       if (!hunted.visible && isOnViewport) {
           hunted.enter.call(this, hunted.element);
           hunted.visible = true;
+
+          // when hunting should not persist remove element
+          if (hunted.out === noop && !hunted.persist) {
+            this._huntedElements.splice(len, 1);
+          
+            // end observer activity when there are no more elements
+            if (this._huntedElements.length === 0) {
+              this.disconnect();
+            }
+          }
         }
 
       /*
