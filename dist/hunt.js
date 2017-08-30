9effd78a9f761870c181668a8677b3ad90de0013
Jere Menichelli <jmenichelli@gmail.com>
2017-08-30 17:03:04 -0300

    Update banner: v4.0.0

--- dist/hunt.js
+++ dist/hunt.js
@@ -1,4 +1,4 @@
-/* Hunt v3.0.2 - 2017 Jeremias Menichelli - MIT License */
+/* Hunt v4.0.0 - 2017 Jeremias Menichelli - MIT License */
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
   typeof define === 'function' && define.amd ? define(factory) :
